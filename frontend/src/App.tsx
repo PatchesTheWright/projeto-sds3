d09ee4ee45ed29a109dac66176c6475605aad024
PatchesTheWright <jonathan.valbe.jb@gmail.com>
2021-05-05 22:21:03 -0300

    basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import NavBar from "components/NavBar"
+import Footer from "components/Footer"
+import DataTable from "components/DataTable"
+import { Fragment } from "react";
+
 function App() {
   return (
-    <div>
+    <>
+    <NavBar />
+    <div className="container">
       <h1 className="text-primary">Olá mundo!</h1>
-      <p>Testing</p>
+      <DataTable />
     </div>
+    <Footer />
+    </>
   );
 }
 
